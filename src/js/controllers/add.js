11e5e4ee6de4e48b40b608deb97f53999913b1f4
Joshua Bradford <jjyet@yahoo.com>
2017-03-16 19:13:29 -0400

    cleaned up code got rid of consoles and extra test buttons

--- src/js/controllers/add.js
+++ src/js/controllers/add.js
@@ -1,18 +1,6 @@
 const SERVER_URL = 'https://arcane-mountain-37553.herokuapp.com';
 
 function Add ($scope, $http, $state){
-  $scope.test2 = function(){
-    console.log('hi from add')
-  };
-
-//   $scope.addContact(){
-// //     $http.post(`${SERVER_URL}/contacts`)
-// //
-// // //put info in body here from form
-// //
-// //     .then(resp => {
-// //       $scope.contacts = resp.data;
-//   };
 
   $scope.addContact = function (data) {
      var url = `${SERVER_URL}/contacts`;
@@ -20,9 +8,6 @@ function Add ($scope, $http, $state){
        $state.go('home');
      });
    };
-
-
-
 }
 
 Add.$inject = ['$scope', '$http', '$state'];
--- src/js/controllers/list.js
+++ src/js/controllers/list.js
@@ -3,7 +3,6 @@ const SERVER_URL = 'https://arcane-mountain-37553.herokuapp.com';
 var info = 1;
 
 function List ($scope, $http, $state){
-//  $scope.currentContact = null;
   $scope.contacts = [];
 
   function init () {
@@ -16,18 +15,9 @@ function List ($scope, $http, $state){
   init();
 
   $scope.listclick = function (id) {
-    //console.log(info)
-    //console.log(info)
     info = id;
     $scope.clickedContact = id;
-    console.log($scope.clickedContact)
-    if(id === 1){  $state.go('home');}
-    if(id === 2){ $state.go('contacts');}
-  };
-
-  $scope.testclick = function () {
-    //$state.go(`contacts`)
-    console.log("hello")
+    $state.go('contacts');
   };
 
 }
